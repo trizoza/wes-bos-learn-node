f632d65f86dad246a3f5f9381de33e84f57427d6
Peter Trizuliak <peter.trizuliak@gmail.com>
2018-05-14 15:56:57 +0100

    create Store model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Pete'
-  next()
-}
-
 exports.homePage = (req, res) => {
-  console.log(req.name)
   res.render('index');
 }
--- start.js
+++ start.js
@@ -12,6 +12,8 @@ mongoose.connection.on('error', (err) => {
 
 // READY?! Let's go!
 
+require('./models/Store');
+
 // Start our app!
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
