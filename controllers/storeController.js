c6585c87c606528a7ef66a1f227afc59ea057fa1
Peter Trizuliak <peter.trizuliak@gmail.com>
2018-05-15 08:53:25 +0100

    add store form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,18 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store')
+
 exports.homePage = (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Home'});
+}
+
+exports.addStore = (req, res) => {
+  res.render('ediStore', {
+    title: 'Add store'
+  });
+}
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
 }
