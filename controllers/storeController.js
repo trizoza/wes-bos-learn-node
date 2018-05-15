09eee7ebc2d29e651670ee0470e3d2be4b792be8
Peter Trizuliak <peter.trizuliak@gmail.com>
2018-05-15 10:06:56 +0100

    add stores preview

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -6,13 +6,18 @@ exports.homePage = (req, res) => {
 }
 
 exports.addStore = (req, res) => {
-  res.render('ediStore', {
+  res.render('editStore', {
     title: 'Add store'
   });
 }
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save();
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Succesfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 }
